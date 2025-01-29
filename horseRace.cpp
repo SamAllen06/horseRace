3bf916db43f7352236fce85c858ea1638faabb3a
SamAllen06 <samgallen06@gmail.com>
2025-01-29 10:18:44 -0500

    started horserace but ran into issue with #include

---
+++ horseRace.cpp
@@ -0,0 +1,24 @@
+//#include <random>
+#include <iostream>
+
+void advance(int horseNum, int* horses);
+void printLane(int horseNum, int* horses);
+int isWinner(int horseNum, int* horses);
+
+int main(){
+	int horses = {0, 0, 0, 0, 0};
+	keepGoing = TRUE;
+	while (keepGoing){
+		int i;
+		for (i = 0; i++; i<5){
+			advance(i, horses);
+			printLane(i, horses);
+			if (isWinner(i, horses){
+				std::cout << i <<" won the race!" << endl;
+				keepGoing = FALSE
+			} //end if statement
+		} //end for loop
+	} //end while loop
+
+	return 0;
+} //end main
