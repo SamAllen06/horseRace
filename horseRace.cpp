0ddb90f4b241e17760ca88a07d955f97047eb33a
SamAllen06 <samgallen06@gmail.com>
2025-01-31 14:37:21 +0000

    got the body of each function fleshed out, contains minor errors

--- horseRace.cpp
+++ horseRace.cpp
@@ -6,19 +6,49 @@ void printLane(int horseNum, int* horses);
 int isWinner(int horseNum, int* horses);
 
 int main(){
-	int horses = {0, 0, 0, 0, 0};
-	keepGoing = TRUE;
+	int horses[] = {0, 0, 0, 0, 0};
+	bool keepGoing = false;
 	while (keepGoing){
 		int i;
 		for (i = 0; i++; i<5){
 			advance(i, horses);
 			printLane(i, horses);
-			if (isWinner(i, horses){
-				std::cout << i <<" won the race!" << endl;
-				keepGoing = FALSE
+			if (isWinner(i, horses)){
+				std::cout << i <<" won the race!" << std::endl;
+				keepGoing = false;
 			} //end if statement
 		} //end for loop
 	} //end while loop
 
 	return 0;
 } //end main
+
+void advance(int &horseNum, int &horses){
+	std::random_device rd;
+	std::uniform_int_distribution<int> dist(0, 1);
+	int isAdvance = dist(rd);
+	if (isAdvance == 1){
+		*horses(horseNum)++;
+	}//end if statement
+}//end advance
+
+void printLane(int &horseNum, int &horses){
+	int i;
+	for (i = 0; i++; i<15){
+		if (i == *horses(horseNum)){
+			std::cout << i;
+		} //end if statement
+		else{
+			std::cout << ".";
+		} //end else statemt
+	} //end for loop
+} //end printLane
+
+bool isWinner(int &horseNum, int &horses){
+	if (*horses(horseNum) == 15){
+		return true;
+	} //end if statement
+	else{
+		return false;
+	} //end else statement
+} //end isWinner
